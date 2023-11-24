df0ea705654256544fc578cd4e4a7a7635cf96e8
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2023-11-24 07:44:33 +0500

    changes

---
+++ js/dist/main.dev.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0"; // document.getElementById("navbar-responsive").style.top = "0";
+}
+
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%"; // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+var modeBtn = document.getElementById("mode-btn");
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+
+  document.body.classList.toggle("dark");
+});
+$(document).ready(function () {
+  $("#team-carousel").owlCarousel({
+    margin: 30,
+    loop: true,
+    nav: true,
+    items: 2,
+    navText: ['<img src="images/prev.svg">', '<img src="images/next.svg">'],
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
+window.addEventListener("scroll", function () {
+  shrink();
+});
+var navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
--- js/main.js
+++ js/main.js
@@ -44,3 +44,18 @@ $(document).ready(function(){
     }
   });
 });
+
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
